939a7ac956f3677bf220ecc4502b7eced778843d
Vinicius Serra <vssm@cin.ufpe.br>
2019-12-05 15:44:35 -0300

    timer

--- src/personal.js
+++ src/personal.js
@@ -1,9 +1,23 @@
 function depositar_quentinha() {
     var user = firebase.auth().currentUser;
     var api = new ApiHTTP();
-    api.post_quentinha(user.uid, function() {
-        window.location.reload();
-    });
+    var start = new Date().getTime();
+    var final = start + 10000;
+
+    var timer = setInterval(function() {
+        var now = new Date().getTime();
+        var t = final-now;
+        if (t >= 0) {
+            var secs = Math.floor(t/1000);
+            document.getElementById("timer-secs").innerHTML = "Deposite sua quentinha na nossa lixeira nos próximos " + secs + " segundos.";
+        }
+    }, 1000);
+
+    setTimeout(function() {
+        api.post_quentinha(user.uid, function() {
+            window.location.reload();
+        });
+    }, 10005); 
 }
 
 function coletar_recompensa() {
